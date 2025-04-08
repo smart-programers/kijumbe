ad4ce0b0420f46fc5b4f77477711ab22a8ec2bab
Revaycolizer <albinuslucas@gmail.com>
2025-04-09 01:27:59 +0300

    Registration Page
    Changes to be committed:
    	modified:   app/_layout.tsx
    	modified:   app/register.tsx
    	modified:   package.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Provider } from 'react-native-paper';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -29,6 +30,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Provider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
@@ -36,6 +38,7 @@ export default function RootLayout() {
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
+      </Provider>
     </ThemeProvider>
   );
 }
--- app/register.tsx
+++ app/register.tsx
@@ -1,46 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, ScrollView, Pressable, useColorScheme } from 'react-native';
-import { useForm, Controller } from 'react-hook-form';
-import { Button, TextInput, Text, useTheme } from 'react-native-paper';
-import { Ionicons } from '@expo/vector-icons';
-import { Link, useRouter } from 'expo-router';
-import { useToast } from 'react-native-toast-notifications';
-import axios from 'axios';
-import { BaseUrl } from '@/api';
-import { PaperSelect } from 'react-native-paper-select';
-import { NormalGet } from '@/actions/helpers';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Pressable,
+  useColorScheme,
+} from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import { Button, TextInput, Text, useTheme } from "react-native-paper";
+import { Ionicons } from "@expo/vector-icons";
+import { Link, useRouter } from "expo-router";
+import { useToast } from "react-native-toast-notifications";
+import axios from "axios";
+import { BaseUrl } from "@/api";
+import { PaperSelect } from "react-native-paper-select";
+import { NormalGet } from "@/actions/helpers";
 
+import {
+  PhoneNumberInput,
+  getCountryByCode,
+} from "react-native-paper-phone-number-input";
 
+const includeCountries = [
+  "AZ",
+  "BD",
+  "CA",
+  "GB",
+  "IN",
+  "NZ",
+  "US",
+  "TR",
+  "TZ",
+  "UG",
+  "KE",
+];
 
 export default function HomeScreen() {
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [loading,setIsLoading]=useState(false)
+  const [loading, setIsLoading] = useState(false);
   const toast = useToast();
   const router = useRouter();
   const colorScheme = useColorScheme();
-  const isDarkMode = colorScheme === 'dark';
+  const isDarkMode = colorScheme === "dark";
   const theme = useTheme();
+  const [countryCode, setCountryCode] = useState<string>("TZ");
+  const [phoneNumber, setPhoneNumber] = useState<string>();
 
-  const { control, handleSubmit, formState: { errors }, trigger, getValues, reset, watch } = useForm({
+  const { name, flag, dialCode } = getCountryByCode(countryCode);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+    getValues,
+    reset,
+    watch,
+  } = useForm({
     defaultValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-    }
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   });
 
-
-
   const onSubmit = async (data: any) => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
       const registrationData = {
         ...data,
+        photoUrl: "",
+        phoneNumber: `${dialCode}${""}${phoneNumber}`,
       };
 
-      const response = await axios.post(`${BaseUrl}/authentication/register`, registrationData);
+      const response = await axios.post(
+        `${BaseUrl}/authentication/register`,
+        registrationData,
+      );
 
       if (response.status === 200) {
         toast.show("Registered successfully", {
@@ -50,7 +87,7 @@ export default function HomeScreen() {
           animationType: "slide-in",
         });
         reset();
-        
+
         router.push("/");
       }
     } catch (error) {
@@ -61,8 +98,8 @@ export default function HomeScreen() {
       //   animationType: "slide-in",
       // });
       console.log(error, BaseUrl);
-    }finally{
-      setIsLoading(false)
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -75,110 +112,165 @@ export default function HomeScreen() {
   };
 
   return (
-    <ScrollView style={{ backgroundColor: isDarkMode ? theme.colors.background : theme.colors.surface }}>
+    <ScrollView
+      style={{
+        backgroundColor: isDarkMode
+          ? theme.colors.background
+          : theme.colors.surface,
+      }}
+    >
       <View style={styles.choppaContainer}>
-        <Text style={[styles.choppa, { color: theme.colors.primary }]}>Kijumbe</Text>
+        <Text style={[styles.choppa, { color: theme.colors.primary }]}>
+          Kijumbe
+        </Text>
 
-        <Text style={[styles.inputContainer, { color: theme.colors.onBackground }]}>First Name</Text>
+        <Text
+          style={[styles.inputContainer, { color: theme.colors.onBackground }]}
+        >
+          First Name
+        </Text>
         <Controller
           control={control}
-          rules={{ required: 'First Name is required' }}
+          rules={{ required: "First Name is required" }}
           render={({ field: { onChange, onBlur, value } }) => (
             <TextInput
               label="First Name"
-              placeholder='Revay'
-              mode='outlined'
+              placeholder="Revay"
+              mode="outlined"
               onBlur={() => {
                 onBlur();
-                trigger('firstName');
+                trigger("firstName");
               }}
               onChangeText={onChange}
               value={value}
               style={styles.input}
               left={<TextInput.Icon icon="account" />}
               theme={{
-                             ...paperSelectTheme,
-                             roundness: 20,
-                           }}
+                ...paperSelectTheme,
+                roundness: 20,
+              }}
             />
           )}
           name="firstName"
         />
-        {errors.firstName && <Text style={styles.errorText}>{errors.firstName.message}</Text>}
-        
-        <Text style={[styles.inputContainer, { color: theme.colors.onBackground }]}>Last Name</Text>
+        {errors.firstName && (
+          <Text style={styles.errorText}>{errors.firstName.message}</Text>
+        )}
+
+        <Text
+          style={[styles.inputContainer, { color: theme.colors.onBackground }]}
+        >
+          Last Name
+        </Text>
         <Controller
           control={control}
-          rules={{ required: 'Last Name is required' }}
+          rules={{ required: "Last Name is required" }}
           render={({ field: { onChange, onBlur, value } }) => (
             <TextInput
               label="Last Name"
-              placeholder='Colizer'
-              mode='outlined'
+              placeholder="Colizer"
+              mode="outlined"
               onBlur={() => {
                 onBlur();
-                trigger('lastName');
+                trigger("lastName");
               }}
               onChangeText={onChange}
               value={value}
               style={styles.input}
               left={<TextInput.Icon icon="account" />}
               theme={{
-                             ...paperSelectTheme,
-                             roundness: 20,
-                           }}
+                ...paperSelectTheme,
+                roundness: 20,
+              }}
             />
           )}
           name="lastName"
         />
-        {errors.lastName && <Text style={styles.errorText}>{errors.lastName.message}</Text>}
+        {errors.lastName && (
+          <Text style={styles.errorText}>{errors.lastName.message}</Text>
+        )}
 
-        <Text style={[styles.inputContainer, { color: theme.colors.onBackground }]}>Email</Text>
+        <Text
+          style={[styles.inputContainer, { color: theme.colors.onBackground }]}
+        >
+          Email
+        </Text>
         <Controller
           control={control}
           rules={{
-            required: 'Email is required',
+            required: "Email is required",
             pattern: {
               value: /^\S+@\S+$/i,
-              message: 'Enter a valid email address',
+              message: "Enter a valid email address",
             },
           }}
           render={({ field: { onChange, onBlur, value } }) => (
             <TextInput
               label="Email"
-              mode='outlined'
-              placeholder='example@gmail.com'
+              mode="outlined"
+              placeholder="example@gmail.com"
               onBlur={() => {
                 onBlur();
-                trigger('email');
+                trigger("email");
               }}
               left={<TextInput.Icon icon="email" />}
               onChangeText={onChange}
               value={value}
               style={styles.input}
               theme={{
-                             ...paperSelectTheme,
-                             roundness: 20,
-                           }}
+                ...paperSelectTheme,
+                roundness: 20,
+              }}
             />
           )}
           name="email"
         />
-        {errors.email && <Text style={styles.errorText}>{errors.email.message}</Text>}
+        {errors.email && (
+          <Text style={styles.errorText}>{errors.email.message}</Text>
+        )}
 
-        <Text style={[styles.inputContainer, { color: theme.colors.onBackground }]}>Password</Text>
+        <Text
+          style={[styles.inputContainer, { color: theme.colors.onBackground }]}
+        >
+          Phone Number
+        </Text>
+        <PhoneNumberInput
+          code={countryCode}
+          setCode={setCountryCode}
+          phoneNumber={phoneNumber}
+          setPhoneNumber={setPhoneNumber}
+          includeCountries={includeCountries}
+          contentStyle={[styles.input, { color: theme.colors.onBackground }]}
+          theme={{
+            ...paperSelectTheme,
+            roundness: 20,
+          }}
+          style={{ color: theme.colors.onBackground}}
+        />
+
+        <Text
+          style={[styles.inputContainer, { color: theme.colors.onBackground }]}
+        >
+          Password
+        </Text>
         <Controller
           control={control}
-          rules={{ required: 'Password is required', minLength: { value: 6, message: 'Password must be at least 6 characters' } }}
+          rules={{
+            required: "Password is required",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters",
+            },
+          }}
           render={({ field: { onChange, onBlur, value } }) => (
             <TextInput
               label="Password"
-              mode='outlined'
-              placeholder='...............'
+              mode="outlined"
+              placeholder="..............."
               secureTextEntry={!passwordVisible}
               onBlur={() => {
                 onBlur();
-                trigger('password');
+                trigger("password");
               }}
               left={<TextInput.Icon icon="lock" />}
               right={
@@ -191,31 +283,38 @@ export default function HomeScreen() {
               value={value}
               style={styles.input}
               theme={{
-                             ...paperSelectTheme,
-                             roundness: 20,
-                           }}
+                ...paperSelectTheme,
+                roundness: 20,
+              }}
             />
           )}
           name="password"
         />
-        {errors.password && <Text style={styles.errorText}>{errors.password.message}</Text>}
+        {errors.password && (
+          <Text style={styles.errorText}>{errors.password.message}</Text>
+        )}
 
-        <Text style={[styles.inputContainer, { color: theme.colors.onBackground }]}>Repeat Password</Text>
+        <Text
+          style={[styles.inputContainer, { color: theme.colors.onBackground }]}
+        >
+          Repeat Password
+        </Text>
         <Controller
           control={control}
           rules={{
-            required: 'Repeat Password is required',
-            validate: value => value === getValues().password || 'Passwords do not match'
+            required: "Repeat Password is required",
+            validate: (value) =>
+              value === getValues().password || "Passwords do not match",
           }}
           render={({ field: { onChange, onBlur, value } }) => (
             <TextInput
               label="Repeat Password"
-              mode='outlined'
-              placeholder='................'
+              mode="outlined"
+              placeholder="................"
               secureTextEntry={!passwordVisible}
               onBlur={() => {
                 onBlur();
-                trigger('confirmPassword');
+                trigger("confirmPassword");
               }}
               left={<TextInput.Icon icon="lock" />}
               right={
@@ -228,21 +327,18 @@ export default function HomeScreen() {
               value={value}
               style={styles.input}
               theme={{
-                             ...paperSelectTheme,
-                             roundness: 20,
-                           }}
+                ...paperSelectTheme,
+                roundness: 20,
+              }}
             />
           )}
           name="confirmPassword"
         />
-        {errors.confirmPassword && <Text style={styles.errorText}>{errors.confirmPassword.message}</Text>}
+        {errors.confirmPassword && (
+          <Text style={styles.errorText}>{errors.confirmPassword.message}</Text>
+        )}
 
-      
-
-        <Pressable
-          style={styles.button}
-          onPress={handleSubmit(onSubmit)}
-        >
+        <Pressable style={styles.button} onPress={handleSubmit(onSubmit)}>
           <Button
             style={[styles.btn, styles.roundedBtn]}
             buttonColor={theme.colors.primary}
@@ -250,7 +346,9 @@ export default function HomeScreen() {
             rippleColor={theme.colors.primaryContainer}
             disabled={loading}
           >
-            <Text style={[styles.text, { color: theme.colors.onPrimary }]}>Register</Text>
+            <Text style={[styles.text, { color: theme.colors.onPrimary }]}>
+              Register
+            </Text>
           </Button>
         </Pressable>
 
@@ -274,11 +372,17 @@ export default function HomeScreen() {
           </Button>
         </View> */}
 
-        <Text style={{ textAlign: "center", color: theme.colors.onBackground }}>Terms and Conditions</Text>
+        <Text style={{ textAlign: "center", color: theme.colors.onBackground }}>
+          Terms and Conditions
+        </Text>
 
-        <Text style={{ color: theme.colors.onBackground }}>Have an account?</Text>
+        <Text style={{ color: theme.colors.onBackground }}>
+          Have an account?
+        </Text>
         <Link href={"/login"}>
-          <Text style={[styles.sign, { color: theme.colors.primary }]}>Sign In</Text>
+          <Text style={[styles.sign, { color: theme.colors.primary }]}>
+            Sign In
+          </Text>
         </Link>
       </View>
     </ScrollView>
@@ -288,29 +392,29 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   titleContainer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     justifyContent: "center",
   },
   button: {
-    paddingVertical: 24
+    paddingVertical: 24,
   },
   input: {
-    width: '100%',
+    width: "100%",
     marginVertical: 10,
-    borderRadius:100
+    borderRadius: 100,
   },
   inputContainer: {
-    width: '100%',
+    width: "100%",
   },
   sign: {
     fontSize: 18,
   },
   btn: {
     flex: 1,
-      alignItems: 'center',
-      justifyContent: "center",
-      height: 50,
-      overflow: 'hidden',
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    overflow: "hidden",
   },
   text: {
     fontSize: 20,
@@ -324,30 +428,30 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingVertical: 80,
     paddingHorizontal: 20,
-    margin: 10
+    margin: 10,
   },
   choppa: {
     fontSize: 70,
     fontWeight: "bold",
-    textAlign: 'center',
+    textAlign: "center",
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
   },
   roundedBtn: {
-     borderRadius: 25,
-   },
+    borderRadius: 25,
+  },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
   container: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginVertical: 20,
   },
   line: {
@@ -360,14 +464,14 @@ const styles = StyleSheet.create({
   },
   btnGoogle: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     justifyContent: "center",
     height: 50,
     borderRadius: 4,
     borderWidth: 2,
   },
   errorText: {
-    color: 'red',
+    color: "red",
     marginTop: 5,
   },
   servicesContainer: {
@@ -377,4 +481,3 @@ const styles = StyleSheet.create({
     borderRadius: 4,
   },
 });
-
